43a13e88df2fce1f9b025d8d07ed4c78f88511e7
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-07-07 13:45:43 -0300

    Fixes props requirement in DefaultChronometer

--- src/components/molecules/DefaultChronometer/DefaultChronometer.vue
+++ src/components/molecules/DefaultChronometer/DefaultChronometer.vue
@@ -3,13 +3,13 @@
         <section clas="timer">
             <strong>{{timeStamp}}</strong>
         </section>
-        <button class="button action-btn" @click="onInit" :disabled="timeStamp !== '00:00:00'">
+        <button class="button action-btn" @click="onInit()" :disabled="timeStamp !== '00:00:00'">
             <span class="icon has-text-success">
                 <i class="fas fa-play"></i>
             </span>
             <span>play</span>
         </button>
-        <button class="button action-btn" @click="onFinish" :disabled="timeStamp == '00:00:00' || !timeStamp">
+        <button class="button action-btn" @click="onFinish()" :disabled="timeStamp == '00:00:00' || !timeStamp">
             <span class="icon has-text-danger">
                 <i class="fas fa-stop"></i>
             </span>
@@ -24,9 +24,9 @@ import { defineComponent } from 'vue';
 export default defineComponent({
     name: 'DefaultChronometer',
     props: {
-        timeStamp: String, 
-        onInit: Function,
-        onFinish: Function,
+        timeStamp: { type: String, required: true }, 
+        onInit: { type: Function, required: true },
+        onFinish: { type: Function, required: true },
     },
     setup(props) {
         props.timeStamp,
