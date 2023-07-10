a2370c966ce29004be6ae50fb9f8e2d0f248e060
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-07-10 00:32:45 -0300

    Implements PanelItem

---
+++ src/components/atoms/PanelItem/PanelItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <a class="panel-block panel-item is-flex is-justify-content-center">
+            <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            <span  class="desc">
+                <strong>{{name || "Tarefa sem descrição"}}</strong>
+            </span>
+            <span class="time-spent">
+                <i class="fas fa-clock" aria-hidden="true"></i>
+                {{timeSpent}}
+            </span>
+        </a>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'PanelItem',
+    props: {
+        name: { type: String, required: true },
+        timeSpent: { type: String, required: true },
+    }
+});
+</script>
+
+<style scoped>
+@import "./style.css";
+</style>
--- src/components/organisms/DefaultPanel/DefaultPanel.vue
+++ src/components/organisms/DefaultPanel/DefaultPanel.vue
@@ -4,18 +4,12 @@
         {{ title }}
     </p>
     <template v-if="list && list?.length > 0">
-        <a class="panel-block panel-item is-flex is-justify-content-center" v-for="item in list" :key="item.name+item.timeSpent">
-            <span class="panel-icon">
-                <i class="fas fa-book" aria-hidden="true"></i>
-            </span>
-            <span  class="mx-3">
-                <strong>{{item.name}}</strong>
-            </span>
-            <span class="time-spent">
-                <i class="fas fa-clock" aria-hidden="true"></i>
-                {{item.timeSpent}}
-            </span>
-        </a>
+        <PanelItem 
+            v-for="(item,index) in list" 
+            :key="index" 
+            :name="item.name"
+            :timeSpent="item.timeSpent"
+        />
     </template>
     <template v-else>
         <a class="panel-block panel-item is-flex is-justify-content-center">
@@ -31,19 +25,21 @@
 <script lang='ts'>
 import { defineComponent } from 'vue';
 import { PropType } from 'vue';
+import PanelItem from '@/components/atoms/PanelItem/PanelItem.vue';
 import ITask from "@/types/ITask";
 
 
 export default defineComponent({
-    name: 'DefaultPanel',
+    name: "DefaultPanel",
     props: {
         title: { type: String, required: true },
-        list:  { type: Array as PropType<ITask[]>, required: true },
+        list: { type: Array as PropType<ITask[]>, required: true },
     },
     setup(props) {
         props.title,
-        props.list
-    }
+            props.list;
+    },
+    components: { PanelItem }
 });
 </script>
 
